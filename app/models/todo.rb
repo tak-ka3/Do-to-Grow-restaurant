b146c1b3408e6b2e44ea6974911f96118a0208af
tak-ka3 <hi.ta9.23.333@gmail.com>
2021-09-19 22:57:41 +0900

    modelsでもバリデーションをつけることで、application側でも制限をつけることで、エラーが出なくなった。#9

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,7 @@
 class Todo < ApplicationRecord
   belongs_to :user # userという名前でアクセスすることができる
+
+  validates :task, presence: true
+  validates :priority, presence: true
+  validates :due, presence: true
 end
