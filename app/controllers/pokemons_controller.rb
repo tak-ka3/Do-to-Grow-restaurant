7d7f1131e4addbd21ee944b5aca4f9df1736f366
tak-ka3 <hi.ta9.23.333@gmail.com>
2021-09-19 17:46:53 +0900

    引く回数もリンクに書くようにした。ログインしていないと、そしてポイントが10溜まっていないと引けないようにした。#2

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,4 +1,5 @@
 class PokemonsController < ApplicationController
+  before_action :authenticate_user!
   def new
   end
 
@@ -7,36 +8,39 @@ class PokemonsController < ApplicationController
   end
 
   def create
-
-    random_id = rand(1..898)
-    url = "https://pokeapi.co/api/v2/pokemon/#{random_id}/"
-    uri = URI.parse(url)
-    response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http| 
-      http.open_timeout = 5
-      http.read_timeout = 10
-      http.get(uri.request_uri)
-    end
-    result = JSON.parse(response.body)
-
-    name = result['name']
-    id = result['id']
-    img = result['sprites']['front_default']
-    pokemon_type = result['types'][0]['type']['name']
-
-    @pokemon = Pokemon.new(name: name, pokemon_type: pokemon_type, img: img, user_id: current_user.id)
-    
-    if @pokemon.save
-      flash[:success] = 'ポケモンをゲットできました。'
-      
-      current_user.update(points: current_user.points - 10)
-      current_user.save
-      
-      redirect_to pokemon_path(@pokemon.id)
+    if current_user.points < 10
+      flash[:danger] = "ポイントが後#{10 - current_user.points}ポイント足りません。"
+      redirect_to todos_path
     else
-      flash[:danger] = 'ポケモンをゲットできませんでした。'
-      redirect_to new_pokemon_path
-    end
+      random_id = rand(1..898)
+      url = "https://pokeapi.co/api/v2/pokemon/#{random_id}/"
+      uri = URI.parse(url)
+      response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http| 
+        http.open_timeout = 5
+        http.read_timeout = 10
+        http.get(uri.request_uri)
+      end
+      result = JSON.parse(response.body)
+
+      name = result['name']
+      id = result['id']
+      img = result['sprites']['front_default']
+      pokemon_type = result['types'][0]['type']['name']
+
+      @pokemon = Pokemon.new(name: name, pokemon_type: pokemon_type, img: img, user_id: current_user.id)
     
+      if @pokemon.save
+        flash[:success] = 'ポケモンをゲットできました。'
+        
+        current_user.update(points: current_user.points - 10)
+        current_user.save
+        
+        redirect_to pokemon_path(@pokemon.id)
+      else
+        flash[:danger] = 'ポケモンをゲットできませんでした。'
+        redirect_to new_pokemon_path
+      end
+    end
   end
 
   def show
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,6 @@
 class TodosController < ApplicationController
   before_action :authenticate_user!
+  
   def new
     @todo = Todo.new
   end
